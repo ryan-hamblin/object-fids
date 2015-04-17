ec4e8fd44a49acf3de134bc3350ac4a0f78e736b
Ryan Hamblin <ryanhca@gmail.com>
2015-04-17 17:47:05 -0600

    meobject

---
+++ fiddles.js
@@ -0,0 +1,11 @@
+//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
+
+//code here
+
+var me = {};
+
+me.name = "Ryan";
+me.age = 28;
+
+alert(me.name);
+
